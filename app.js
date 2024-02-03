565489d5a669ef18964c02d0f7fbfcf819777b0c
Maarcbaz <138406453+Maarcbaz@users.noreply.github.com>
2024-02-03 12:27:00 +0100

    initial commit

--- app.js
+++ app.js
@@ -29,3 +29,62 @@ let popupDisplay1 = document.querySelector(".popMenu")
 function DisplayPopup() {
     popupDisplay1.classList.toggle("active")
 }
+let popMenu_project = document.querySelector('.popmenu_project')
+function displayPopupMsg() {
+    popMenu_project.classList.toggle("active_popup_project")
+}
+let increases = document.querySelectorAll(".increase")
+let decreases = document.querySelectorAll(".decrease")
+let inPuts = document.querySelectorAll(".number")
+inPuts.forEach(input => {
+    input
+    // inPuts.style.color = "var(--BLACK)"
+    increases.forEach(increase => {
+       increase.addEventListener("click", () => {
+            input.innerHTML++
+        })
+    })
+    decreases.forEach(decrease => {
+        decrease.addEventListener("click", () => {
+            input.innerHTML--
+        })
+    
+    })
+})
+// GETTING EACH MAIN ELEMENT 
+let popupRewards = document.querySelectorAll(".popupReward")
+popupRewards.forEach(popupReward => {
+    popupReward.addEventListener("click", () => {
+        popupReward.style.border = "1px solid var(--DARK-CYAN)"
+
+        // let rewards = document.querySelectorAll(".pledge_section")
+        // console.log(rewards);
+        // rewards.forEach(reward => {
+        //     reward.style.display = "block"
+        // });
+        // for (let i = 0; i < rewards.length; i++) {
+        //     rewards.style.display = "block"
+        // }
+    })
+});
+// let ttxs = document.querySelectorAll(".rewardType")
+// let i, j;
+// for (i = 0; i < ttxs.length; i++) {
+//     ttxs[i]
+//     console.log(ttxs[i]);
+//     console.log(ttxs[i].textContent)
+//     let btnOpts = document.querySelectorAll(".btnCon")
+//     btnOpts.forEach(btnOpt => {
+//     })
+// }
+
+let btns = document.querySelectorAll(".btnS")
+btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        alert("Thanks for supporting our project")
+    })
+});
+
+let btnO = document.querySelector(".btnO").addEventListener("click", () => {
+    alert("This produt is no more available")
+})
